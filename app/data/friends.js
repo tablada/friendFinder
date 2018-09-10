710762b2f7a364192f375c8cc9417e55586acdcc
Raul <raultablada@yahoo.com>
2018-09-10 00:09:29 -0400

    html

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friendArray = [
+    {
+        "name": "Kanye",
+        "photo": "",
+        "scores": [5, 4, 5, 3, 5, 4, 2, 5, 4, 3]
+    },
+    {
+        "name": "Kim",
+        "photo": "",
+        "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        "name": "Drake",
+        "photo": "",
+        "scores": [4, 3, 2, 5, 4, 2, 1, 3, 4, 5]
+    },
+    {
+        "name": "Jay",
+        "photo": "",
+        "scores": [5, 4, 3, 4, 5, 3, 2, 4, 5, 4]
+    },
+    {
+        "name": "Bey",
+        "photo": "",
+        "scores": [3, 4, 5, 4, 3, 5, 4, 4, 3, 4]
+    }
+]
+
+module.exports = friendArray;
