b816c3d7be07aa6ed91b7efa56119c77e827e2b3
Raul <raultablada@yahoo.com>
2018-10-05 00:07:02 -0400

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var bodyParser = require("body-parser");
 var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 
-var app  = express();
+var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+var jsonParser = bodyParser.json()
 
-var PORT = 3306;
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+app.use(bodyParser.json({ type: 'application/*+json'}))
+
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}))
+
+app.use(bodyParser.text({ type: 'text/html'}))
 
 app.listen(PORT, function() {
-    console.log("Friend Finder on http://localhost:" + PORT);
+    console.log("Friend Finder listening on PORT: " + PORT);
 });
